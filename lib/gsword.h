a31f0e63fdcd90b9104f765de107b1d46c449ae1
Xiaotian Wu <yetist@gmail.com>
2023-01-02 22:01:58 +0800

    update

---
+++ lib/gsword.h
@@ -0,0 +1,33 @@
+/* vi: set sw=4 ts=4 wrap ai: */
+/*
+ * gsword.h: This file is part of ____
+ *
+ * Copyright (C) 2017 yetist <yetist@yetibook>
+ *
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * */
+
+#ifndef __GSWORD_H__ 
+#define __GSWORD_H__  1
+
+#include <gsword/gsw-installer.h>
+#include <gsword/gsw-manager.h>
+#include <gsword/gsw-modinfo.h>
+#include <gsword/gsw-module.h>
+#include <gsword/gsw-search-hit.h>
+#include <gsword/gsw-status-reporter.h>
+
+#endif /* __GSWORD_H__ */
